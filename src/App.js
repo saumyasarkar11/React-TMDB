fe7aca92997f560da8f93d5d44b61438d16a21c5
Saumya Sarkar <saumyasarkar27@gmail.com>
2022-02-23 19:14:58 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { GlobalStyle } from "./GlobalStyle";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import Api from "../Api";
+
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+};
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await Api.fetchMovies(searchTerm, page);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  return { state, loading, error };
+};
